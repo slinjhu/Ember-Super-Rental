4bbaaa18f9d0410f0110c32cbf3b117a06b171b4
Sen Lin <slin@jhu.edu>
2017-04-11 00:01:33 -0700

    Added actions.

---
+++ tests/integration/components/rental-listing-test.js
@@ -0,0 +1,11 @@
+import { moduleForComponent, test } from 'ember-qunit';
+//import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('rental-listing', 'Integration | Component | rental listing', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // TODO: add test
+  assert.equal('', '');
+});
